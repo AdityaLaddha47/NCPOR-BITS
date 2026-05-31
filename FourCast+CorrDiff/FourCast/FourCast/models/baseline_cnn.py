cd45af254d71ca8e3c17a0a52fd28478dee0813f
LaZyMugen <shaswatsahoo234@gmail.com>
2026-05-31 13:00:22 +0530

    Add baseline CNN and preprocessing pipeline

--- FourCast+CorrDiff/FourCast/FourCast/models/baseline_cnn.py
+++ FourCast+CorrDiff/FourCast/FourCast/models/baseline_cnn.py
@@ -1,2 +1,23 @@
-class BaselineCNN:
-    pass
+import torch
+import torch.nn as nn
+
+class BaselineCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 4, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x):
+        B,T,C,H,W = x.shape
+
+        x = x.reshape(B, T*C, H, W)
+
+        return self.net(x)
